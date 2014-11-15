5d62be09c4204f1c670858b5123c91d732edf73f
DominikJS <ssnd0038@humbermail.ca>
2014-11-15 17:53:24 -0500

    Assignment3

---
+++ DominikSosin_AndyNaranjo_Assignment3_PHP/header.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<title>BLUEASY</title>
+		<meta name="author" content="Dominik Sosin">
+		<meta charset="utf-8">
+		<link rel="icon" href="favicon.ico" type="image/x-icon">
+		<link rel="stylesheet" href="styles/normalize.css" type="text/css">
+		<link rel="stylesheet" href="styles/main.css" type="text/css">
+		<link rel="stylesheet" href="styles/fonts.css" type="text/css">
+		<link rel="stylesheet" href="styles/font-awesome.css">
+		<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+		<script src="script/main.js"></script>
+		<script src="script/jquery.hovercap.js"></script>
+		<meta name="viewport" content="initial-scale=1, maximum-scale=1">
+	</head>
+
+	<body>
+
+		<header>
+			
+			<nav>
+				<div class="container clearfix">
+
+					<h1>BLU<span class="blue">E</span>ASY</h1>
+			
+					<span class="fa fa-bars main fa-2x" title="menu"></span>
+					<ul class="nav dropdown">	
+						<li><a href="#contact">CONTACT</a></li>
+						<li><a href="#portfolio">PORTFOLIO</a></li>
+						<li><a href="#features">FEATURES</a></li>
+						<li><a href="#services">SERVICES</a></li>
+						<li><a href="javascript:scroll(0,0)">HOME</a></li>					
+					</ul>
+
+				</div>
+			</nav>
+
+			<div class="container clearfix">
+				<h2><span class="highlight">“I’M LOOKING FOR THE UNEXPECTED.</span></h2> 
+				<h2><span class="highlight">I’M LOOKING FOR THINGS I’VE NEVER SEEN BEFORE.”</span></h2>
+			</div>
+			
+		</header>
