6ae0ca2a3b956cd89ec85ca669de3c0a2fcd2b8a
Pragmatismx <kc.bunnaman@gmail.com>
2014-11-16 16:44:08 -0500

    submit

---
+++ bunnaman-casy/webd221-assignment3/assets/js/script.js
@@ -0,0 +1,8 @@
+//responsive menu
+$('.menu-btn').click(function(){
+
+  $('.responsive-menu').toggleClass('expand')
+  console.log("menu clicked")
+  $('.menu-btn').toggleClass('close-btn')
+
+})
---
+++ bunnaman-casy/webd221-assignment3/contact-footer.php
@@ -0,0 +1,63 @@
+<section id="contact">
+    <h1>Let's discuss</h1>
+    <i class="fa fa-heart-o"><span></span></i>
+    <p>Voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore</p>
+    <form method="post" action="#">
+       <div>
+        <p>Cras at ultrices erat, sed vulputate!</p>
+        <p>2345 Setwant natrer, 1234, <br>Washington. United States.<br>(401) 1234 567<br>hello@brandi.com <br>ww.brandi.com</p>
+      </div>
+      <div>
+      <h2>Say hello!</h2>
+        <label class="label">Name*</label>
+        <input type="text" name="name" value="" size="40" class="" required="true" placeholder="Name">
+        <label class="label">Email*</label>
+        <input type="text" name="Email" value="" size="40" class="" required="true" placeholder="Email">
+        <label class="label">Message*</label>
+        <textarea type="textarea" name="Message" value="" size="40" class="" required="true" placeholder="Message"></textarea>
+        <button name="submit" type="submit" value="submit"><i class="fa fa-envelope-o"></i> Send message</button>
+      </div>      
+    </form>
+    <aside>
+      <ul>
+        <li><a href="#"><i class="fa fa-3x fa-behance"></i></a></li>
+        <li><a href="#"><i class="fa fa-3x fa-twitter"></i></a></li>
+        <li><a href="#"><i class="fa fa-3x fa-dribbble"></i></a></li>
+        <li><a href="#"><i class="fa fa-3x fa-facebook"></i></a></li>
+        <li><a href="#"><i class="fa fa-3x fa-google-plus"></i></a></li>        
+      </ul>
+    </aside>
+    <img src="assets/images/map.jpg" alt="" id="map">
+  </section><!--Closing contact section-->
+
+  <footer id="footer">
+      <div>
+        <h1>Brandi</h1>
+        <p>eusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+      </div>
+      <div>
+        <h2>Subscribe</h2>
+        <form method="post" action="#" class="subscribe">
+          <label class="label">subscribe*</label>
+          <input type="text" name="subscribe" value="" size="40" class="" required="true" placeholder="your email">
+        </form>
+        <p>eusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+      </div>
+      <div>
+        <h2>Explore</h2>
+        <ul>
+          <li><a href="#">Envato</a></li>
+          <li><a href="#">Themecurve</a></li>
+          <li><a href="#">Kreativeshowcase</a></li>
+          <li><a href="#">Freebiescurve</a></li>
+          <li><a href="#">Themeforest</a></li>
+          <li><a href="#">Microsoft</a></li>
+          <li><a href="#">Google</a></li>
+          <li><a href="#">Yahoo</a></li>
+        </ul>
+      </div>
+      <div>
+        <p>Created with <i class="fa fa-heart-o"></i> By <a href="#">themecurve</a>, exclusive for kreativeshowcase</p>
+        <p>© 2014 Brandi. All Rights Reserved.</p>
+      </div>
+  </footer><!--Closing footer--> 
