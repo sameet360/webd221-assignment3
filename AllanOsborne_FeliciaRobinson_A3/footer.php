8a03d53e8f2f777be123a66454bc2aa223272b4f
A-Osborne <allan-osborne@hotmail.com>
2014-11-15 23:59:39 -0500

    adding assignment files

---
+++ AllanOsborne_FeliciaRobinson_A3/footer.php
@@ -0,0 +1,13 @@
+		<footer id="footer-bottom" class="clearfix">
+			<div class="container">
+				<ul class="footer-nav">
+					<li><a href="#">Home</a> |</li>
+					<li><a href="#">Services</a>  |</li>
+					<li><a href="#">Portfolio</a>  |</li>
+					<li><a href="#">Contact</a></li>
+				</ul>
+				<p>2013 &copy;</p>
+			</div> 	<!-- /container -->
+		</footer>
+	</body>
+</html>
---
+++ AllanOsborne_FeliciaRobinson_A3/header.php
@@ -0,0 +1,31 @@
+<!DOCTYPE HTML>
+<html>
+	<head>
+		<title>Assignment 3: Blue Easy</title>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<link href="//maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet">
+		<link rel="stylesheet" href="css/main.css">
+	</head>
+
+	<body>
+      <header>
+        <div id="mainNav">
+          <div class="container">
+            <a href="index.php"><img class="logo" src="img/logo.png" alt=""/></a>
+            <nav>
+              <ul>
+                <li><a href="#home">Home</a></li>
+                <li><a href="#services">Services</a></li>
+                <li><a href="#features">Features</a></li>
+                <li><a href="#portfolio">Portfolio</a></li>
+                <li><a href="#contact">Contact</a></li>
+              </ul>
+            </nav>
+          </div>  <!--  /CONTAINER   -->
+        </div>
+        <div id="bottomHeader">
+			<h1>"I'M LOOKING FOR THE UNEXPECTED."</h1>
+			<h1>"I'M LOOKING FOR THINGS I'VE NEVER SEEN BEFORE."</h1>
+        </div>
+      </header>
