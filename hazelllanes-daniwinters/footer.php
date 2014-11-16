330ad467a83d7729e7655794f9862e8e06dee395
hzlllns <hello@hzl.ca>
2014-11-16 13:39:43 -0500

    final files

---
+++ hazelllanes-daniwinters/footer.php
@@ -0,0 +1,27 @@
+		<footer>
+			<div class="container">
+				<nav>
+					<ul>
+						<li><a href="#">Home</a></li>
+						<li><a href="#">Services</a></li>
+						<li><a href="#">Portfolio</a></li>
+						<li><a href="#">Contact</a></li>
+					</ul>
+					<p class="copyright">2013 &copy;</p>
+				</nav>
+			</div>
+		</footer><!-- /footer -->
+
+		<!-- jQuery magic starts here -->
+		<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+		<script>
+		$(document).ready(function(){
+			$(".fa-bars").on("click", function(){
+				$(".dropdown").toggleClass("open");
+				console.log("SDKGJB;sdgjn");
+			});
+		});
+		</script>
+
+	</body>
+	</html>
--- hazelllanes-daniwinters/index.php
+++ hazelllanes-daniwinters/index.php
@@ -1,43 +1,4 @@
-<!Doctype HTML>
-<html>
-
-<head>
-	<title>BluEasy</title>
-	<meta charset="utf-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<meta name="description" content="Portfolio website for WEBD 221">
-	<meta name="author" content="Hazel (the awesome) Llanes and Dani (the amazing) Winters">
-
-	<link rel="stylesheet" type="text/css" href="css/normalize.css">
-	<link rel="stylesheet" type="text/css" href="css/style.css">
-	<link href='http://fonts.googleapis.com/css?family=PT+Sans:400,700,400italic,700italic' rel='stylesheet' type='text/css'>
-	<link href="http://maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet">
-
-	<script type="text/javascript" src="main.js"></script>
-</head>
-
-<body>
-	<header>
-		<div id="topbar">
-			<div class="container">
-				<h1><a href="#">Blu<span class="brightE">E</span>asy</a></h1>
-				<nav>
-					<span class="fa fa-bars fa-2x" title="menu"></span>
-					<ul class="dropdown">
-						<li><a href="#" class="current">Home</a></li>
-						<li><a href="#">Services</a></li>
-						<li><a href="#">Features</a></li>
-						<li><a href="#">Portfolio</a></li>
-						<li><a href="#">Contact</a></li>
-					</ul>
-				</nav>
-			</div>
-		</div><!-- #topbar -->
-
-		<div class="container header">
-			<h2>&#8220;I&#8217;m looking for the unexpected.<br> I&#8217;m looking for things I&#8217;ve never seen before.&#8221;</h2>
-		</div>
-	</header><!-- /header -->
+<?php include "header.php"; ?>
 
 	<section id="services">
 		<div class="container">
@@ -208,35 +169,4 @@
 			</div>
 		</section><!--/#contact-->
 
-		<footer>
-			<div class="container">
-				<nav>
-					<ul>
-						<li><a href="#">Home</a></li>
-						<li><a href="#">Services</a></li>
-						<li><a href="#">Portfolio</a></li>
-						<li><a href="#">Contact</a></li>
-					</ul>
-					<p class="copyright">2013 &copy;</p>
-				</nav>
-			</div>
-		</footer><!-- /footer -->
-
-		<!-- jQuery magic starts here -->
-		<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
-		<script>
-		$(document).ready(function(){
-			$(".fa-bars").on("click", function(){
-				$(".dropdown").toggleClass("open");
-				console.log("SDKGJB;sdgjn");
-			});
-		});
-		</script>
-
-	</body>
-	</html>
-
-
-
-
-
+<?php include "footer.php"; ?>
