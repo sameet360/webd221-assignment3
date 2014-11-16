c78d7a2f74af08705eef8294a481e298f64f3c3e
wesleydean <fez_w@hotmail.com>
2014-11-16 00:02:45 -0500

    Assignment3 Wes , Marsha

---
+++ includes/footer.php
@@ -0,0 +1,46 @@
+        <section id="footer">
+	        <div class="container">
+	        	<ul>
+	        		<li>
+	        			<img src="images/logo.png">
+	        			<h5>eusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</h5>
+	        		</li>
+	        		<li>
+	        			<h5 class="bold">Subscribe</h5>
+	        			<input/>
+	        			<h5>eusmod tempor incididunt ut labore et dolore magna aliqua.</h5>
+	        		</li>
+	        		<li id="explore">
+
+	        			<h5 class="bold">Explore</h5>
+
+	        			<ul>
+	        				<h5><a href="#">Envato</a></h5>	
+	        				<h5><a href="#">Themecurve</a></h5>	
+	        				<h5><a href="#">Kreativeshowcase</a></h5>	
+	        				<h5><a href="#">Freebiescurve</a></h5>	
+	        			</ul>
+	        			<ul>
+	        				<h5><a href="#">Themeforesta</a></h5>	
+	        				<h5><a href="#">Microsofta</a></h5>	
+	        				<h5><a href="#">Google</a></h5>	
+	        				<h5><a href="#">Yahoo</a></h5>	
+	        			</ul>
+
+	        		</li>
+	        		<li>
+	        			<div id="theme-blurb" class="bold"><h5>Created with <span class="fa fa-heart-o"></span> by <span id="navy-text">themecurve</span>, exclusive for kreativeshowcase</h5></div>
+	        			<h5>&copy; 2014 Brandi. All Rights Reserved.</h5>
+	        		</li>
+	        	</ul>
+	        </div>
+        </section>
+
+</div>
+</body>
+<!-- JQUERY PLUGINS BELOW -->
+<script src="http://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
+<script src="js/jquery.bxslider.min.js"></script> 
+<script src="js/main.js"></script>
+
+</html>
