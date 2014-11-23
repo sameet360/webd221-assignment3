34d305084f438de5365cd1fcb9720234c3d8c460
SameetDhaliwal <sameetd2@gmail.com>
2014-11-23 14:00:03 -0500

    adding assignment files

---
+++ sameetandtalha/styles/header.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+<head>
+	
+	<meta charset="UTF-8">
+	<title>Blueeasy</title>
+	
+	<link href='http://fonts.googleapis.com/css?family=PT+Sans' rel='stylesheet' type='text/css'>
+	
+	<link rel="stylesheet" href="styles/normalize.css" type="text/css" media="screen" />
+	<link rel="stylesheet" href="styles/main.css" type="text/css" media="screen" />
+
+</head>
+	<body>
+
+		<header>
+			<nav class="main-nav">
+				<div class="container">
+					<ul>
+						<li><a href=""class"bordernav">Home</a></li>
+						<li><a href="" class"bordernav">Services</a></li>
+						<li><a href="" class"bordernav">Features</a></li>
+						<li><a href="" class"bordernav">Portfolio</a></li>
+						<li><a href="" class"bordernav">Contact</a></li>
+					</ul>
+				</div>
+		</header>
